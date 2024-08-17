4310ebc3fd4d8ba046e62b47d64bc608cf0fcce8
Zander <chuang.master@gmail.com>
2024-08-18 01:47:16 +0800

    加入專案檔案。

---
+++ SPA-JWT-Sample/Controllers/AuthenticationController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPA_JWT_Sample.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        private IConfiguration Configuration;
+        public AuthenticationController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        /// <summary>
+        /// use login model to validate and generate jwt token into cookie
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login(LoginModel loginModel)
+        {
+            if (loginModel.Username == "admin" && loginModel.Password == "password")
+            {
+                var token = GenerateJwtToken(Configuration);
+                var cookieOptions = new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true, // 在開發環境中可以設置為 false，生產環境中應設置為 true (目前測試下來都使用true並使用https做傳遞成功率比較高)
+                    SameSite = SameSiteMode.None, // 確保跨站點 cookie 可以被發送
+                };
+                Response.Cookies.Append("access_token", token, cookieOptions);
+                return Ok("Login success");
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        private string GenerateJwtToken( IConfiguration config)
+        {
+            // Generate JWT token logic here
+            
+            return "generated_token";
+        }
+    }
+
+
+    public class LoginModel
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ SPA-JWT-Sample/Program.cs
@@ -0,0 +1,48 @@
+
+namespace SPA_JWT_Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            // Add Cors to allow requests from the SPA app
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseCors();
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
