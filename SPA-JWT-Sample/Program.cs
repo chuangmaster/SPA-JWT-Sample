01f8a4ee6b3ba7106b5899d9d44af1959116f073
Zander <chuang.master@gmail.com>
2024-08-19 00:23:26 +0800

    Add Jwt service, middleware

--- SPA-JWT-Sample/Program.cs
+++ SPA-JWT-Sample/Program.cs
@@ -1,15 +1,18 @@
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 namespace SPA_JWT_Sample
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
-            // Add Cors to allow requests from the SPA app
+            // Add cors service
             builder.Services.AddCors(options =>
             {
                 options.AddDefaultPolicy(
@@ -20,7 +23,19 @@ namespace SPA_JWT_Sample
                             .AllowAnyMethod();
                     });
             });
-
+            // Add jwt service
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"] ?? "default secret"))
+                    };
+                });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -34,7 +49,9 @@ namespace SPA_JWT_Sample
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            app.UseCors();
+            app.UseCors("AllowSpecificOrigin");
+            //use jwt service
+
             app.UseHttpsRedirection();
 
             app.UseAuthentication();
